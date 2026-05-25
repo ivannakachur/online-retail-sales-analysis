e6e3a718f3a6e5095f4a875a95560a6de6467c6b
ivannakachur <ivannakachur7@gmail.com>
2026-05-25 18:11:19 +0300

    Update data_cleaning.sql

--- sql/data_cleaning.sql
+++ sql/data_cleaning.sql
@@ -1,6 +1,8 @@
+-- Check number of records
 select count(*)
 from private.retail;
 
+-- Create cleaned dataset
 create table private.retail_clean as
 select distinct *
 from private.retail
@@ -8,6 +10,7 @@ where invoice not like 'C%'
 and quantity > 0
 and customer_id is not null;
 
+-- Create revenue column
 alter table private.retail_clean 
 alter column revenue type numeric(10, 2);
 
